fa09ee4d3f72feeceb3c30b06c6b82d85b9fc120
Akash Srivastava <akashsrivastava@Akashs-MacBook-Pro.local>
2021-06-16 14:23:16 +0530

    Hosting

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,7 @@
 import withHOC from "./withHOC";
 
 function NotFound() {
-    return <h2>Oops, Please see you have typed the correct link.</h2>
+    return <h2>Oops, Please check if you have typed the correct link.</h2>
 }
 
 export default withHOC(NotFound);
