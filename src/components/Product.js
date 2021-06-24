a834b9fe274e8a3ccf4207070adc7d0b61ad16de
Akash Srivastava <akashsrivastava@Akashs-MacBook-Pro.local>
2021-06-24 21:55:52 +0530

    Add to cart functionality added from product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Product(props) {
   const { product, onAdd } = props;
-  const prodDetails = (product) => {
+  const history = useHistory();
+  
+  useEffect(() => {
+    let prodAdd = document.getElementById("prodAdd");
+    prodAdd && prodAdd.addEventListener("click", function(){
+      window.alert("Product has been added in the cart");
+    }, false);
+
+  })
+  const prodDetails = (product, onAdd) => {
     if (product) {
       let url = window.location.origin === "http://localhost:3000" ? '/#/prodDetails' : 'https://akashsrivastava1993.github.io/shopping-cart/#/prodDetails';
-      let abc = window.open(url);
-      abc.image = product.image;
-      abc.name = product.name;
-      abc.price = product.price;
+      let newWindow = window.open(url);
+      newWindow.image = product.image;
+      newWindow.name = product.name;
+      newWindow.price = product.price;
+      newWindow.click = product;
+      newWindow.onAdd = onAdd;
     }
   }
   console.log(window.location.origin, "JMS");
   return (
     <div>
-      <img className="small" src={window.image || product.image} alt={window.name || product.name} onClick={() => prodDetails(product)} />
+      <img className="small" src={window.image || product.image} alt={window.name || product.name} onClick={() => prodDetails(product, onAdd)} />
       <h3>{window.name || product.name}</h3>
       <div>₹{window.price || product.price}</div>
       <div>
-        {product ? <button onClick={() => onAdd(product)}>Add To Cart</button> : ""}
+       {product ? <button onClick={() => onAdd(product || window.click)}>Add To Cart</button> :
+       <button id = "prodAdd" onClick={() => window.onAdd(window.click)}>Add To Cart</button>}
       </div>
     </div>
   );
