cd6b55cc660c9547e90067405e747e2ca6cc3766
Akash Srivastava <akashsrivastava@Akashs-MacBook-Pro.local>
2021-06-24 19:04:45 +0530

    Added prodDetails on local as well

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,12 +4,14 @@ export default function Product(props) {
   const { product, onAdd } = props;
   const prodDetails = (product) => {
     if (product) {
-      let abc = window.open('https://akashsrivastava1993.github.io/shopping-cart/#/prodDetails')
+      let url = window.location.origin === "http://localhost:3000" ? '/#/prodDetails' : 'https://akashsrivastava1993.github.io/shopping-cart/#/prodDetails';
+      let abc = window.open(url);
       abc.image = product.image;
       abc.name = product.name;
       abc.price = product.price;
     }
   }
+  console.log(window.location.origin, "JMS");
   return (
     <div>
       <img className="small" src={window.image || product.image} alt={window.name || product.name} onClick={() => prodDetails(product)} />
